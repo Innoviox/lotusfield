424be25239cbebfbaf4a699f651758ce6ee6ce34
Simon Chervenak <sarras305@gmail.com>
2022-06-24 13:10:27 -0400

    implement routing

--- lotusfield/src/app/app-routing.module.ts
+++ lotusfield/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DeckDetailComponent} from "./deck-detail/deck-detail.component";
+import {DeckComponent} from "./deck/deck.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'decks/:id', component: DeckDetailComponent },
+  { path: 'decks', component: DeckComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- lotusfield/src/app/app.module.ts
+++ lotusfield/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DeckComponent } from './deck/deck.component';
+import { DeckDetailComponent } from './deck-detail/deck-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DeckComponent,
+    DeckDetailComponent,
   ],
   imports: [
     BrowserModule,
